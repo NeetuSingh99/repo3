e30ec4569ccaeb2135737b0fa24029b85e27da7a
minakshid <minakshid1>
2020-02-27 16:27:05 +0530

    changed

--- add.java
+++ add.java
@@ -7,5 +7,11 @@ int b=10;
 int sum=a+b;
 System.out.println("sum is"+sum);
 System.out.println("hello github");
+System.out.println("how are you doing there");
+System.out.println("sum is"+sum);
+System.out.println("hello github");
+System.out.println("sum is"+sum);
+System.out.println("hello github");
+
 }
 }
